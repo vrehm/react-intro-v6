fb82abedf69788d09cf7f345a4a30dbcaf9fcd2d
Vincent REHM <contact@vincentrehm.fr>
2022-01-28 15:53:56 +0100

    jsx setup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import Pet from "./pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Thelma",
-      animal: "Cat",
-      breed: "European Shorthair",
-    }),
-    React.createElement(Pet, {
-      name: "Louise",
-      animal: "Cat",
-      breed: "European Longhair",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="my-brand">Adopt Me!</h1>
+      <Pet name="Thelma" animal="Cat" breed="European Shorthair" />
+      <Pet name="Louise" animal="Cat" breed="European Longhair" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,11 @@
-import React from "react";
+const Pet = ({ name, animal, breed }) => {
+  return (
+    <div>
+      <h1>Meet {name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+};
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-}
+export default Pet;
