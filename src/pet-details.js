3f5287fe143ec2d8bc4d7572afd90838da80db62
Vincent REHM <contact@vincentrehm.fr>
2022-02-03 16:23:35 +0100

    class properties

--- src/pet-details.js
+++ src/pet-details.js
@@ -2,10 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class PetDetails extends Component {
-  constructor() {
-    super();
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
