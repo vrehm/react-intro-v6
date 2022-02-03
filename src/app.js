07c10e98299715a2c940b35228ca26b7a4d81879
Vincent REHM <contact@vincentrehm.fr>
2022-02-03 15:22:25 +0100

    react router

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import { render } from "react-dom";
 import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./search-params";
+import PetDetails from "./pet-details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Switch>
+          <Route path={`/details/:id`} component={PetDetails} />
+          <Route path="/" component={SearchParams} />
+        </Switch>
+      </Router>
     </div>
   );
 };
