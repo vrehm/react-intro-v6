cca0e964c04a58a96651a836815289bf51cbb0fa
Vincent REHM <contact@vincentrehm.fr>
2022-02-03 12:41:06 +0100

    handling user input => submit

--- src/search-params.js
+++ src/search-params.js
@@ -5,7 +5,7 @@ import useBreedList from "./use-breed-list";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, updateLocation] = useState("");
+  const [location, updateLocation] = useState("Paris, France");
   const [animal, updateAnimal] = useState("");
   const [breed, updateBreed] = useState("");
   const [pets, setPets] = useState([]);
@@ -13,7 +13,7 @@ const SearchParams = () => {
 
   useEffect(() => {
     requestPets();
-  }, [animal]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
     const res = await fetch(
@@ -25,7 +25,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
